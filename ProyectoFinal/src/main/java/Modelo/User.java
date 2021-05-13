827c9e074c9ad842d48aa001f8c2c63e73673c02
Abdias <abdias.sanchezgr@uanl.edu.mx>
2021-05-12 19:34:03 -0500

    Modelo Usuarios

---
+++ ProyectoFinal/src/main/java/Modelo/User.java
@@ -0,0 +1,87 @@
+
+package Modelo;
+
+public class User {
+    private String User_name;
+    private String User_lastName;
+    private String User_birthday;
+    private String User_email;
+    private String User_image;
+    private String User_user;
+    private String User_pass;
+    private boolean User_status;
+    private String User_dateCreation;
+
+    public String getUser_name() {
+        return User_name;
+    }
+
+    public void setUser_name(String User_name) {
+        this.User_name = User_name;
+    }
+
+    public String getUser_lastName() {
+        return User_lastName;
+    }
+
+    public void setUser_lastName(String User_lastName) {
+        this.User_lastName = User_lastName;
+    }
+
+    public String getUser_birthday() {
+        return User_birthday;
+    }
+
+    public void setUser_birthday(String User_birthday) {
+        this.User_birthday = User_birthday;
+    }
+
+    public String getUser_email() {
+        return User_email;
+    }
+
+    public void setUser_email(String User_email) {
+        this.User_email = User_email;
+    }
+
+    public String getUser_image() {
+        return User_image;
+    }
+
+    public void setUser_image(String User_image) {
+        this.User_image = User_image;
+    }
+
+    public String getUser_user() {
+        return User_user;
+    }
+
+    public void setUser_user(String User_user) {
+        this.User_user = User_user;
+    }
+
+    public String getUser_pass() {
+        return User_pass;
+    }
+
+    public void setUser_pass(String User_pass) {
+        this.User_pass = User_pass;
+    }
+
+    public boolean isUser_status() {
+        return User_status;
+    }
+
+    public void setUser_status(boolean User_status) {
+        this.User_status = User_status;
+    }
+
+    public String getUser_dateCreation() {
+        return User_dateCreation;
+    }
+
+    public void setUser_dateCreation(String User_dateCreation) {
+        this.User_dateCreation = User_dateCreation;
+    }
+        
+}
